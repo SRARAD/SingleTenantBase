d730cf21a47ff2ef316ac05da6a013b9c3e375b5
theconnman <bcconn2112@gmail.com>
2014-06-26 12:59:44 -0400

    Added AWS email to config.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -59,6 +59,19 @@ grails.exceptionresolver.params.exclude = ['password']
 // configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
 grails.hibernate.cache.queries = false
 
+grails {
+	mail {
+		host='email-smtp.us-east-1.amazonaws.com'
+		port=465
+		props = [
+			'mail.smtp.auth': 'true',
+			'mail.smtp.socketFactory.port':'465',
+			"mail.smtp.socketFactory.class": "javax.net.ssl.SSLSocketFactory",
+			"mail.smtp.socketFactory.fallback": "false"
+		]
+	}
+}
+
 environments {
     development {
         grails.logging.jul.usebridge = true
