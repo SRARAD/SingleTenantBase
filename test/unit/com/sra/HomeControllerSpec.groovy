df3919707bd4f4f3975f106a30cc1f626837fe1b
TheConnMan <bcconn2112@gmail.com>
2015-12-10 13:19:36 -0500

    Cleanup tests

--- test/unit/com/sra/HomeControllerSpec.groovy
+++ test/unit/com/sra/HomeControllerSpec.groovy
@@ -1,25 +1,9 @@
 package com.sra
 
-import grails.test.mixin.TestFor
-import spock.lang.*
+import grails.test.mixin.*
+import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
 @TestFor(HomeController)
 class HomeControllerSpec extends Specification {
 
-    def setup() {
-    }
-
-    def cleanup() {
-    }
-
-    void "test change password"() {
-		when:
-			controller.changePassword();
-		
-		then:
-			view == '/home/chpass'
-    }
 }
